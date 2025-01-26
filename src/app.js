adf381237c2dc9e834390ef05cf9ce67d8ec575a
comdeb52 <compton582@gmail.com>
2025-01-26 14:33:14 +0000

    City and temp updated using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function reviseWeather(response) {
+  let cityElement = document.querySelector("#selected-city");
+  let city = response.data.city;
+  let temperatureElement = document.querySelector("#callout-temp");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7t430af5962198oce5dea3144944bb3b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(reviseWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchCityInput = document.querySelector("#search-city-input");
-  let cityElement = document.querySelector("#selected-city");
-  cityElement.innerHTML = searchCityInput.value;
+  searchCity(searchCityInput.value);
 }
 
 let citySearchElement = document.querySelector("#city-search-form");
 citySearchElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
